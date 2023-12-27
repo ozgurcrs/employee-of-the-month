3d6293e1d0bc2025975e43f9071649a9a785d99a
Özgür Çiriş <ozgurciris@Ozgurs-MacBook-Pro.local>
2023-12-27 04:01:56 +0300

    initial project

---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Container } from "./style";
+
+export const Navbar: FC = () => {
+  return (
+    <Container>
+      <h1>Employee of Month</h1>
+    </Container>
+  );
+};
---
+++ src/app/components/Navbar/style.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.nav`
+  width: 100%;
+  height: 65px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 24px;
+
+  & > h1 {
+    font-size: 48px;
+    font-weight: 200;
+  }
+`;
+
+export { Container };
