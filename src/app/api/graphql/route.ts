1dcd8eb7ef93173d54b9d2e0f39bfc8e9332a13f
Özgür Çiriş <ozgurciris@Ozgurs-MacBook-Pro.local>
2023-12-27 12:09:07 +0300

    set server side route from nextjs engine

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,43 @@
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { NextRequest } from "next/server";
+import { gql } from "graphql-tag";
+import { employees } from "@/app/mock";
+
+const typeDefs = gql`
+  type Employee {
+    id: Int
+    firstName: String
+    lastName: String
+    title: String
+    voteCount: Int
+    image: String
+    email: String
+    address: String
+    telefon: String
+  }
+
+  type Query {
+    employees: [Employee]
+    employee(id: Int!): Employee
+  }
+`;
+
+const resolvers = {
+  Query: {
+    employees: () => employees,
+    employee: (_: any, { id }: any) =>
+      employees.find((employee) => employee.id === id),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async (req) => ({ req }),
+});
+
+export { handler as GET, handler as POST };
