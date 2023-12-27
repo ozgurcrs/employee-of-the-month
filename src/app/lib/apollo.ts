655d1940f73e9ae4d99eb25b8a795d04fcb99e53
Özgür Çiriş <ozgurciris@Ozgurs-MacBook-Pro.local>
2023-12-27 18:50:07 +0300

    vercel fix v2

--- src/app/lib/apollo.ts
+++ src/app/lib/apollo.ts
@@ -1,13 +1,7 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-import fetchPonyfill from "fetch-ponyfill";
-
-const httpLink = new HttpLink({
-  fetch: fetchPonyfill().fetch,
-  uri: `http://127.0.0.1:3000/api/graphql`,
-});
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: httpLink,
+  uri: `http://localhost:3000/api/graphql`,
   cache: new InMemoryCache(),
 });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import { Cards } from "./components/Cards";
-import client from "@/app/lib/apollo";
 import { GET_EMPLOYEES } from "./queries";
 import { ApiResponse, Employee } from "./types/employee";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: `http://localhost:3000/api/graphql`,
+  cache: new InMemoryCache(),
+});
 
 export default async function Home() {
   const data: ApiResponse = await client.query({
